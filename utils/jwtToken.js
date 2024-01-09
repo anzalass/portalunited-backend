16b30e83b00a82ae9bbbdabc9ef559c64167f745
anzalass <109114576+anzalass@users.noreply.github.com>
2024-01-09 18:39:11 +0700

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -5,6 +5,8 @@ const sendToken = (user, statusCode, res) => {
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    sameSite: 'None', // Adjust based on your requirements and SSL setup
+    secure: true,     // Make sure to set this to true if using HTTPS
   };
   res.status(statusCode).cookie("token_blog", token_blog, options).json({
     success: true,
